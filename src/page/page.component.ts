abad50c924f1ff0da1c2a5361831ce9c46739a64
Wing Liang <wing.liang@agileact.com>
2017-12-25 18:14:25 +0800

    完善详细课堂导航

--- src/page/page.component.ts
+++ src/page/page.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener} from '@angular/core';
 import { Imgs } from '../assets/cont/carouse.imgs';
 
+
 @Component({
-  selector: 'app-page',
-  templateUrl: './page.component.html',
-  styleUrls: ['./page.component.scss']
+    selector: 'app-page',
+    templateUrl: './page.component.html',
+    styleUrls: ['./page.component.scss']
 })
 export class PageComponent {
-  liClick=false;
+
+    /**
+     * 定义导航条上课堂和机构的切换
+     */
+    liClick = false;
+    isSearch = false;
+    isMyClass=false;
+    searchClass: string = "课堂";
+    searchOrgan: string = "机构";
+    serarchOne: string;
+
+    searchSwitch() {
+        this.serarchOne = this.searchClass;
+        this.searchClass = this.searchOrgan;
+        this.searchOrgan = this.serarchOne;
+        this.isSearch = false;
+    }
 
 
-  
 }
